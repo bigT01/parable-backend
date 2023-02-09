fb5bfba330caf8cae0139234a6fe75559ad0da33
Tanat <tanat8511@gmail.com>
2023-02-09 14:46:25 +0600

    init

---
+++ index.js
@@ -0,0 +1,75 @@
+import express from 'express';
+import fs from 'fs';
+import multer from 'multer';
+import cors from 'cors';
+
+import mongoose from 'mongoose';
+
+import { registerValidation, loginValidation, postCreateValidation } from './validations.js';
+
+import { handleValidationErrors, checkAuth } from './utils/index.js';
+
+import { UserController, PostController } from './controllers/index.js';
+import {getByTag, getUpdateLike} from "./controllers/PostController.js";
+
+mongoose
+    .connect("mongodb+srv://Tanat:Tan311309tan@cluster0.epmao20.mongodb.net/?retryWrites=true&w=majority")
+    .then(() => console.log('DB ok'))
+    .catch((err) => console.log('DB error', err));
+
+const app = express();
+
+const storage = multer.diskStorage({
+    destination: (_, __, cb) => {
+        if (!fs.existsSync('uploads')) {
+            fs.mkdirSync('uploads');
+        }
+        cb(null, 'uploads');
+    },
+    filename: (_, file, cb) => {
+        cb(null, file.originalname);
+    },
+});
+
+const upload = multer({ storage });
+
+app.use(express.json());
+app.use(cors());
+app.use('/uploads', express.static('uploads'));
+
+app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login);
+app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register);
+app.get('/auth/me', checkAuth, UserController.getMe);
+
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+    res.json({
+        url: `/uploads/${req.file.originalname}`,
+    });
+});
+
+app.patch('/profile/about/:id', checkAuth, UserController.update)
+
+app.get('/tags', PostController.getLastTags);
+
+app.get('/posts', PostController.getAll);
+app.get('/posts/tags', PostController.getLastTags);
+app.get('/posts/:tag', PostController.getByTag);
+app.patch('/post/:id', PostController.getLastTags);
+app.get('/postsl/:id',checkAuth, PostController.getUpdateLike);
+app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
+app.delete('/posts/:id', checkAuth, PostController.remove);
+app.patch(
+    '/posts/:id',
+    checkAuth,
+    postCreateValidation,
+    handleValidationErrors,
+    PostController.update,
+);
+
+app.listen(process.env.PORT || 4444, (err) => {
+    if (err) {
+        return console.log(err);
+    }
+
+    console.log('Server OK');
+});
